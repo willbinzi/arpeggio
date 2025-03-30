113554361b6b7d23503f274f7284cc9256c1c08f
Will Binzi <will.binzi@moia.io>
2025-03-30 22:36:17 +0200

    Use more consistent argument naming in assertApproxEqual

--- core/src/test/scala/arpeggio/ArpeggioSuite.scala
+++ core/src/test/scala/arpeggio/ArpeggioSuite.scala
@@ -7,12 +7,12 @@ import munit.CatsEffectSuite
 trait ArpeggioSuite extends CatsEffectSuite:
   // We're using streams of floats so approximate equality is the best we can hope for
   def assertApproxEqual(
-      obtainedStream: Stream[IO, Float],
+      obtained: Stream[IO, Float],
       expected: Stream[Pure, Float],
       tolerance: Float = 0.00000001f
   ): IO[Unit] =
     assertIOBoolean(
-      obtainedStream
+      obtained
         .zipWith(expected)(_ - _)
         .forall(delta => Math.abs(delta) < tolerance)
         .compile
