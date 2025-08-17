c8faaa8ccd2af1eefa8ee65fc35dc313af264dfd
Will Binzi <willbinzi@gmail.com>
2025-08-17 14:51:00 +0200

    Add soft clipping overdrive

--- pedals/src/main/scala/arpeggio/pedals/overdrive/package.scala
+++ pedals/src/main/scala/arpeggio/pedals/overdrive/package.scala
@@ -5,11 +5,23 @@ import arpeggio.pedals.volume.adjustLevel
 import arpeggio.routing.parallel
 import cats.effect.Concurrent
 
-def asymmetricClipping[F[_]](threshold: Float): Pedal[F] =
+def asymmetricHardClipping[F[_]](threshold: Float): Pedal[F] =
   _.map(sample => Math.min(sample, threshold))
 
-def symmetricClipping[F[_]](threshold: Float): Pedal[F] =
+def symmetricHardClipping[F[_]](threshold: Float): Pedal[F] =
   _.map(sample => Math.min(Math.max(sample, -threshold), threshold))
+
+def softClipping[F[_]](threshold: Float): Pedal[F] =
+  _.map {
+    case sample if sample > threshold  => threshold
+    case sample if sample < -threshold => -threshold
+    case 0f                            => 0f
+    case sample if sample > 0 =>
+      // Take a "normalised distance" from the threshold and raise it to the power 4 to "softly" decrease it
+      threshold * (1 - Math.pow(1 - (sample / threshold), 4)).toFloat
+    case sample if sample < 0 =>
+      threshold * (-1 + Math.pow(1 + (sample / threshold), 4)).toFloat
+  }
 
 def blended[F[_]: Concurrent](
     blend: Float,
@@ -17,5 +29,5 @@ def blended[F[_]: Concurrent](
 ): Pedal[F] =
   parallel(
     adjustLevel(1 - blend),
-    symmetricClipping(threshold) andThen adjustLevel(blend)
+    symmetricHardClipping(threshold) andThen adjustLevel(blend)
   )
